54dc0dd1deb47cb2736a03831151aa7192428d20
obinnaEd <e.agbassi@stu.unizik.edu.ng>
2025-09-30 13:01:05 +0100

    modified WashFold import

--- src/components/WashFoldSection.vue
+++ src/components/WashFoldSection.vue
@@ -60,7 +60,7 @@
     </div>
   </section>
 
-  <HowItWorksection />
+  <HowItWorksSection />
   <fee-section />
   <delivery-section />
   <pricing-section />
@@ -78,7 +78,7 @@
 
 <script setup>
 import BaseButton from "@/components/atoms/BaseButton.vue";
-import HowItWorksection from "@/components/HowItWorksSection.vue";
+import HowItWorksSection from "@/components/HowItWorksSection.vue";
 import FeeSection from "@/components/FeeSection.vue";
 import DeliverySection from "@/components/DeliverySection.vue";
 import PricingSection from "@/components/PricingSection.vue";
